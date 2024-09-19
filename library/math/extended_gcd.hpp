50b7b402aba7439f071133769ff7a02a87e4d77b
Tia Shi Wei <tiashiwei04@gmail.com>
2024-09-19 23:34:33 +0800

    Use bigger data type for inv_gcd

--- library/math/extended_gcd.hpp
+++ library/math/extended_gcd.hpp
@@ -6,7 +6,9 @@
 namespace maomao90 {
     template <signed_integral T>
     constexpr T inv_gcd(T x, T mod) {
-        T a = mod, b = x, va = 0, vb = 1;
+        using U = conditional_t<numeric_limits<T>::max() <= numeric_limits<int>::max(), long long, __int128>;
+
+        U a = mod, b = x, va = 0, vb = 1;
         while (b) {
             T k = a / b;
             a -= k * b; va -= k * vb;
