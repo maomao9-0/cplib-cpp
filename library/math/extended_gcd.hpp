243c1bf5879f450185551e17c81ff764276e2338
maomao9-0 <64779547+maomao9-0@users.noreply.github.com>
2024-09-19 23:42:46 +0800

    Reformatted extended_gcd.hpp

--- library/math/extended_gcd.hpp
+++ library/math/extended_gcd.hpp
@@ -9,7 +9,6 @@ namespace maomao90 {
     template <signed_integral T>
     constexpr T inv_gcd(T x, T mod) {
         using U = conditional_t<numeric_limits<T>::max() <= numeric_limits<int>::max(), long long, __int128>;
-
         U a = mod, b = x, va = 0, vb = 1;
         while (b) {
             T k = a / b;
