1cdea645f4c00e7f37cb2dce6649671dacac13e2
Tia Shi Wei <tiashiwei04@gmail.com>
2024-10-22 13:58:04 +0800

    Added type_trait safely_multipliable

--- library/internal/type_traits.hpp
+++ library/internal/type_traits.hpp
@@ -3,19 +3,29 @@
 #include <type_traits>
 #include <limits>
 
-namespace internal::type_traits {
+namespace maomao90::internal::type_traits {
+#define ENABLE_VALUE(x) \
+    template <typename T> \
+    constexpr bool x##_v = x<T>::value
+#define ENABLE_TYPE(x) \
+    template <typename T> \
+    using x##_t = typename x<T>::type
+
     template <typename T>
     using is_32bit_or_less = bool_constant<numeric_limits<T>::max() <= numeric_limits<unsigned int>::max()>;
+    ENABLE_VALUE(is_32bit_or_less);
 
     template <typename T>
     using is_64bit_or_less = bool_constant<numeric_limits<T>::max() <= numeric_limits<unsigned long long>::max()>;
-
-#define ENABLE_VALUE(x) \
-    template <typename T> \
-    constexpr bool x##_v = x<T>::value;
-
-    ENABLE_VALUE(is_32bit_or_less);
     ENABLE_VALUE(is_64bit_or_less);
+
+    template <typename T>
+    using safely_multipliable_unsigned = conditional<is_32bit_or_less_v<T>, unsigned long long, unsigned __int128>;
+    ENABLE_TYPE(safely_multipliable_unsigned);
+
+    template <typename T>
+    using safely_multipliable = conditional<is_signed_v<T>, make_signed_t<safely_multipliable_unsigned_t<T>>, safely_multipliable_unsigned_t<T>>;
+    ENABLE_TYPE(safely_multipliable);
 
 #undef ENABLE_VALUE
 }
