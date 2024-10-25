ee5873642dc80d0d5b6d1573fd3f58a836421ceb
Tia Shi Wei <tiashiwei04@gmail.com>
2024-10-25 19:07:47 +0800

    Add broadly_integral concept

--- library/internal/concepts.hpp
+++ library/internal/concepts.hpp
@@ -2,7 +2,18 @@
 
 #include <concepts>
 
+#include "library/internal/type_traits.hpp"
+
 namespace maomao90::internal::concepts {
+    template <typename T>
+    concept broadly_integral = internal::type_traits::is_broadly_integral_v<T>;
+
+    template <typename T>
+    concept broadly_signed = internal::type_traits::is_broadly_signed_v<T>;
+
+    template <typename T>
+    concept broadly_unsigned = internal::type_traits::is_broadly_unsigned_v<T>;
+
     template <typename T>
     concept Addable = requires(T a) {
         a + a;
