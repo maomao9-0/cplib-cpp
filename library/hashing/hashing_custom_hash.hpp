279d9fad7a4ac66e4db16384ae33803cd65c8b4e
Tia Shi Wei <tiashiwei04@gmail.com>
2024-10-25 19:40:29 +0800

    Utilised hash_function in hashing_custom_hash

--- library/hashing/hashing_custom_hash.hpp
+++ library/hashing/hashing_custom_hash.hpp
@@ -8,20 +8,12 @@
 namespace maomao90 {
     template <typename T>
     concept Hashing = requires(T v) {
-        { T::get_num_bases() } -> convertible_to<size_t>;
         { v.get_v() } -> same_as<array<typename T::mint::umod_type, T::get_num_bases()>>;
     };
     template <Hashing T>
     struct HashingCustomHash {
-        inline static const array<unsigned long long, T::get_num_bases()> fixed_random = 
-            internal::hashing::gen_fixed_random<T::get_num_bases()>();
-        size_t operator()(const T &o) const {
-            unsigned long long res = 0;
-            auto v = o.get_v();
-            for (int i = 0; i < T::get_num_bases(); i++) {
-                res ^= splitmix64(v[i] ^ fixed_random[i]);
-            }
-            return res;
+        constexpr size_t operator()(const T &o) const {
+            return internal::hashing::hash_function(o.get_v());
         }
     };
 }
--- library/internal/hashing.hpp
+++ library/internal/hashing.hpp
@@ -11,7 +11,7 @@ namespace maomao90::internal::hashing {
     const int MIN_HASH_BASE = 128;
     template <typename mint, size_t num_bases>
     constexpr array<mint, num_bases> gen_bases() {
-        mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+        static mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
         array<mint, num_bases> res;
         for (int i = 0; i < num_bases; i++) {
             res[i] = mint::raw(rng() % (mint::umod() - MIN_HASH_BASE) + MIN_HASH_BASE);
@@ -34,15 +34,6 @@ namespace maomao90::internal::hashing {
             for (int j = 1; j < CACHE; j++) {
                 res[i][j] = res[i][j - 1] * bases[i];
             }
-        }
-        return res;
-    }
-    template <size_t num_bases>
-    constexpr array<unsigned long long, num_bases> gen_fixed_random() {
-        mt19937_64 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
-        array<unsigned long long, num_bases> res;
-        for (int i = 0; i < num_bases; i++) {
-            res[i] = rng();
         }
         return res;
     }
