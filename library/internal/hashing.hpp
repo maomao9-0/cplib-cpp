6ee4f88d25b2635292dab95fdc03d24ccb185288
Tia Shi Wei <tiashiwei04@gmail.com>
2024-10-25 18:51:45 +0800

    Lowercase ull

--- library/internal/hashing.hpp
+++ library/internal/hashing.hpp
@@ -47,15 +47,15 @@ namespace maomao90::internal::hashing {
     template <typename T>
     constexpr unsigned long long hash_function(const T &x) {
         static unsigned long long r = chrono::high_resolution_clock::now().time_since_epoch().count();
-        constexpr unsigned long long z1 = 11995408973635179863ULL;
+        constexpr unsigned long long z1 = 11995408973635179863ull;
         if constexpr (internal::type_traits::is_broadly_integral_v<T>) {
             return ((unsigned long long) x ^ r) * z1;
         } else if constexpr (internal::type_traits::is_pair<T>) {
-            constexpr unsigned long long z2 = 10150724397891781847ULL;
+            constexpr unsigned long long z2 = 10150724397891781847ull;
             return hash_function(x.first) + hash_function(x.second) * z2;
         } else if constexpr (internal::concepts::Iterable<T>) {
             constexpr unsigned long long mod = (1LL << 61) - 1;
-            constexpr unsigned long long base = 950699498548472943ULL;
+            constexpr unsigned long long base = 950699498548472943ull;
             unsigned long long m = 0;
             for (auto& z : x) {
                 unsigned long long w;
