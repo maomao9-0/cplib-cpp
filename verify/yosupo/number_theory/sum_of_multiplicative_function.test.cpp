f6e2e7795428e50fda85043e1f6c11775f430781
Tia Shi Wei <=>
2024-11-18 19:21:51 +0800

    Removed unnecessary includes and changed indentation width

--- verify/yosupo/number_theory/sum_of_multiplicative_function.test.cpp
+++ verify/yosupo/number_theory/sum_of_multiplicative_function.test.cpp
@@ -1,13 +1,12 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/sum_of_multiplicative_function_large"
+#define PROBLEM                                                                \
+  "https://judge.yosupo.jp/problem/sum_of_multiplicative_function_large"
 
 #include <iostream>
 #include <vector>
-#include <utility>
 using namespace std;
 
-#include "library/math/multiplicative_function.hpp"
-#include "library/math/poly.hpp"
 #include "library/math/modint.hpp"
+#include "library/math/multiplicative_function.hpp"
 using namespace maomao90;
 using mint = static_modint<469762049>;
 
@@ -16,36 +15,35 @@ long long n;
 int a, b;
 
 int main() {
-    cin >> t;
-    while (t--) {
-        cin >> n >> a >> b;
-        long long m = 1;
-        while ((m + 1) * (m + 1) <= n) {
-            m++;
-        }
-        vector<mint> sumg_0(m * 2), sumg_1(m * 2);
-        for (int i = 1; i <= m; i++) {
-            sumg_0[i - 1] = i - 1;
-            sumg_1[i - 1] = (long long) i * (i + 1) / 2 - 1;
-            sumg_0[2 * m - i] = n / i - 1;
-            sumg_1[2 * m - i] = mint(n / i) * (n / i + 1) / 2 - 1;
-        }
-        vector<mint> sumgp_0 = lucy_dp(n, sumg_0, [&] (long long p) {
-                return mint::raw(1);
-                });
-        vector<mint> sumgp_1 = lucy_dp(n, sumg_1, [&] (long long p) {
-                return mint(p);
-                });
-        vector<mint> sumfp(m * 2);
-        for (int i = 0; i < 2 * m; i++) {
-            sumfp[i] = sumgp_0[i] * a + sumgp_1[i] * b;
-        }
-        auto f = [&] (long long p, int e) {
-            if (p == 1) return mint(1);
-            return mint(a) * e + mint(b) * p;
-        };
-        vector<mint> sumf = min25_sieve(n, sumfp, f);
-        cout << sumf.back() << '\n';
+  cin >> t;
+  while (t--) {
+    cin >> n >> a >> b;
+    long long m = 1;
+    while ((m + 1) * (m + 1) <= n) {
+      m++;
     }
-    return 0;
+    vector<mint> sumg_0(m * 2), sumg_1(m * 2);
+    for (int i = 1; i <= m; i++) {
+      sumg_0[i - 1] = i - 1;
+      sumg_1[i - 1] = (long long)i * (i + 1) / 2 - 1;
+      sumg_0[2 * m - i] = n / i - 1;
+      sumg_1[2 * m - i] = mint(n / i) * (n / i + 1) / 2 - 1;
+    }
+    vector<mint> sumgp_0 =
+        lucy_dp(n, sumg_0, [&](long long p) { return mint::raw(1); });
+    vector<mint> sumgp_1 =
+        lucy_dp(n, sumg_1, [&](long long p) { return mint(p); });
+    vector<mint> sumfp(m * 2);
+    for (int i = 0; i < 2 * m; i++) {
+      sumfp[i] = sumgp_0[i] * a + sumgp_1[i] * b;
+    }
+    auto f = [&](long long p, int e) {
+      if (p == 1)
+        return mint(1);
+      return mint(a) * e + mint(b) * p;
+    };
+    vector<mint> sumf = min25_sieve(n, sumfp, f);
+    cout << sumf.back() << '\n';
+  }
+  return 0;
 }
