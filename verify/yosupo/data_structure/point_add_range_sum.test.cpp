13e9edcbb09731990c89f5a990f3f801622184a1
Tia Shi Wei <=>
2025-06-24 23:01:55 +0800

    Added tests for segtree

---
+++ verify/yosupo/data_structure/point_add_range_sum.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include <iostream>
+using namespace std;
+
+#include "library/data_structure/segtree.hpp"
+#include "library/data_structure/standard_monoids.hpp"
+using namespace maomao90;
+
+using ll = long long;
+using ST = SegTree<SumMonoid<ll>>;
+
+int n, q;
+
+int main() {
+  ios::sync_with_stdio(0), cin.tie(0);
+  cin >> n >> q;
+  vector<SumMonoid<ll>> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i].v;
+  }
+  ST seg(a);
+  while (q--) {
+    int t;
+    cin >> t;
+    if (t == 0) {
+      int p, x;
+      cin >> p >> x;
+      seg.set(p, {seg.get(p).v + x});
+    } else {
+      int l, r;
+      cin >> l >> r;
+      r--;
+      cout << seg.qry(l, r).v << '\n';
+    }
+  }
+  return 0;
+}
