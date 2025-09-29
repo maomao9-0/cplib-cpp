6648b0dbb0d0ecaa1c55f27a34002175e1389899
Tia Shi Wei <=>
2025-09-29 23:24:44 +0800

    Changed static top tree to 0-indexed, and added assertions and documentation.

--- library/tree/static_top_tree.hpp
+++ library/tree/static_top_tree.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 #include "library/internal/concepts.hpp"
@@ -9,33 +10,108 @@ using namespace std;
 
 // 1-indexed
 // parent_path.merge(child_path)
+/**
+ * A static top tree supporting updates and queries on a tree.
+ *
+ * Each vertex of the tree has a value. The static top tree can answer queries
+ * on the aggregation of values in the subtree of a vertex, and also supports
+ * updates to a single vertex value in \f$O(\log_2 n)\f$ time. Note that all
+ * indices are 0-indexed.
+ *
+ * @tparam Path the @ref internal::concepts::Monoid "monoid" storing information
+ *     of path clusters. Should implement `Path(T x)` constructor for a single
+ *     vertex value, and `add_edge()` method to convert to `Point`. Note that
+ *     the `merge` function is called using `parent_path.merge(child_path)`.
+ * @tparam Point the @ref internal::concepts::Monoid "monoid" storing
+ *     information of point clusters. Should implement `add_vertex(T x)` method
+ *     to convert to `Path` by including parent vertex value.
+ * @tparam T the type of the vertex value.
+ * @see @ref internal::concepts::StaticTopTreePathPoint "StaticTopTreePathPoint"
+ */
 template <typename Path, typename Point, typename T = int>
   requires internal::concepts::StaticTopTreePathPoint<Path, Point, T>
 struct StaticTopTree {
   StaticTopTree() {}
-  StaticTopTree(int n, vector<vector<int>> &adj)
-      : n(n), adj(adj), lc(4 * n + 5), rc(4 * n + 5), p(4 * n + 5),
-        op(4 * n + 5), path(4 * n + 5, Path::id()),
-        point(4 * n + 5, Point::id()) {
-    ptr = n + 1;
-    hld(1, -1);
-    r = build_path(1).first;
+  /**
+   * Initialises the static top tree with a tree consisting of `n` vertices
+   * with edges represented by adjacency list `adj`.
+   *
+   * The vertex values are left uninitialised. The index of the vertices in
+   * `adj` should be 0-indexed.
+   *
+   * @param n the number of vertices of the tree.
+   * @param adj the adjacency list representing the edges of the tree.
+   * @pre `adj.size() == n`
+   */
+  StaticTopTree(int n, const vector<vector<int>> &adj)
+      : n(n), adj(adj), lc(4 * n, -1), rc(4 * n, -1), p(4 * n, -1), op(4 * n),
+        path(4 * n, Path::id()), point(4 * n, Point::id()) {
+    assert((int)adj.size() == n);
+    ptr = n;
+    hld(0, -1);
+    r = build_path(0).first;
   }
-  void init(vector<T> &_a) {
-    a = _a;
+  /**
+   * Initialises the static top tree with a tree consisting of `n` vertices with
+   * values represented by array `a` and edges represented by adjacency list
+   * `adj`.
+   *
+   * The index of the vertices in `adj` and array `a` should be 0-indexed.
+   *
+   * @param n the number of vertices of the tree.
+   * @param adj the adjacency list representing the edges of the tree.
+   * @param a the values of the vertices.
+   * @pre `adj.size() == n`
+   * @pre `a.size() == n`
+   */
+  StaticTopTree(int n, const vector<vector<int>> &adj, const vector<int> &a)
+      : StaticTopTree(n, adj) {
+    assert((int)a.size() == n);
+    this->a = a;
     dfs(r);
   }
-  void update(int u, T &x) {
+  /**
+   * Set the vertex values to be equal to array `a`.
+   *
+   * Runs in \f$O(n)\f$ time.
+   *
+   * @param a the values of the vertices.
+   * @pre `a.size() == n`
+   */
+  void init(const vector<T> &a) {
+    assert((int)a.size() == n);
+    this->a = a;
+    dfs(r);
+  }
+  /**
+   * Set the value of vertex `u` (0-indexed) to be equal to `x`.
+   *
+   * @param u the index of the vertex to update.
+   * @param x the new value to update to vertex `u`.
+   *
+   * @pre `0 <= u < n`.
+   */
+  void update(int u, const T &x) {
+    assert(0 <= u && u < n);
     a[u] = x;
     apply(u);
-    while (p[u]) {
+    while (p[u] != -1) {
       u = p[u];
       apply(u);
     }
   }
+  /**
+   * Query the aggregation of values in the subtree of vertex `u`
+   * (0-indexed).
+   *
+   * @param u the index of the vertex to query.
+   *
+   * @pre `0 <= u < n`.
+   */
   Path query(int u) {
+    assert(0 <= u && u < n);
     Path res = path[u];
-    while (p[u]) {
+    while (p[u] != -1) {
       if (op[p[u]] == AddEdge) {
         break;
       }
@@ -57,8 +133,6 @@ private:
   vector<Op> op;
   vector<Path> path;
   vector<Point> point;
-  bool has_rc(int u) { return op[u] == Compress || op[u] == Rake; }
-  bool is_point(int u) { return op[u] == AddEdge || op[u] == Rake; }
   int hld(int u, int p) {
     int sub = 1, mx = 0;
     for (int i = 0; i < (int)adj[u].size(); i++) {
@@ -80,10 +154,10 @@ private:
     return sub;
   }
   void dfs(int u) {
-    if (lc[u]) {
+    if (lc[u] != -1) {
       dfs(lc[u]);
     }
-    if (rc[u]) {
+    if (rc[u] != -1) {
       dfs(rc[u]);
     }
     apply(u);
@@ -103,9 +177,11 @@ private:
   }
   inline void add(int u, int l, int r, Op o) {
     lc[u] = l;
-    p[l] = u;
     rc[u] = r;
-    p[r] = u;
+    p[l] = u;
+    if (r != -1) {
+      p[r] = u;
+    }
     op[u] = o;
   }
   pair<int, int> merge(vector<pair<int, int>> &lst, Op o) {
@@ -145,12 +221,12 @@ private:
       lst.push_back(build_edge(adj[u][i]));
     }
     auto [v, s] = merge(lst, Rake);
-    add(u, v, 0, AddVertex);
+    add(u, v, -1, AddVertex);
     return {u, s + 1};
   }
   pair<int, int> build_edge(int u) {
     auto [v, s] = build_path(u);
-    add(ptr, v, 0, AddEdge);
+    add(ptr, v, -1, AddEdge);
     return {ptr++, s + 1};
   }
 };
